6c2f32bccf914cfd3168f55a46278ebbafbf0095
dhvik <dan.handevik@gmail.com>
2013-04-09 10:38:29 +0200

    Renamed KillLine command

--- DocumentationVs12AddIn/Commands/MiscCommands.cs
+++ DocumentationVs12AddIn/Commands/MiscCommands.cs
@@ -2,13 +2,12 @@
 
 namespace DocumentationVs12AddIn.Commands {
 	/// <summary>
-	/// Summary description for KillLineCommand.
+	/// Summary description for MiscCommands.
 	/// </summary>
 	/// <remarks>
 	/// 2013-04-08 dan: Created
 	/// </remarks>
-	
-	public class KillLineCommand:CommandBase {
+	public class MiscCommands:CommandBase {
 		[Command("Text Editor::Ctrl+Shift+k")]
 		public void KillLine() {
 			if (Selection == null)
